7a9798a74817166b266ba184453e6bee4e74ab89
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-03 14:36:30 -0300

    Modify styles and make inputs adapt width slightly

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Start");
+
+const submitButton = document.querySelector("#submit-button");
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#password-confirm");
+
+passwordConfirm.addEventListener("input", checkPassword);
+
+function checkPassword() {
+    if(password.value === passwordConfirm.value) console.log("Same");
+    else console.log("Not same");
+}
